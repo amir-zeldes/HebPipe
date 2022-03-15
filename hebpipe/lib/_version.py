36f23e9b1d9c022b4f9afde06bb11b22e69d2eb6
amir-zeldes <amir.zeldes@georgetown.edu>
2022-03-15 09:22:27 -0400

    version 2.0.0

--- hebpipe/lib/_version.py
+++ hebpipe/lib/_version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = "1.0.0.3"
+__version__ = "2.0.0.0"
 __author__ = "Amir Zeldes"
-__copyright__ = "Copyright 2018-2020, Amir Zeldes"
+__copyright__ = "Copyright 2018-2022, Amir Zeldes"
 __license__ = "Apache 2.0 License"
